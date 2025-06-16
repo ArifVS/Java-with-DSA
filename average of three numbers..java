5ced7fd83fae7ffdfabb795b35f4f47df3491e43
Arif <v.s.arifulla@gmail.com>
2025-06-17 00:21:23 +0530

    Create average of three numbers..java

---
+++ average of three numbers..java
@@ -0,0 +1,11 @@
+// with Arguments, No return type
+import java.util.*;
+class Main{
+    static void Avg(int a, int b, int c){
+        double average = (a+b+c)/3.0;
+        System.out.print(average);
+    }
+    public static void main(String[] args){
+        Avg(10,20,30);
+    }
+}
