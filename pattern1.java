1b4e4fccf07d25e2fea549d49db956815c6e6ca2
Arif <v.s.arifulla@gmail.com>
2025-06-27 23:43:54 +0530

    Update pattern1.java

--- pattern1.java
+++ pattern1.java
@@ -18,3 +18,22 @@ class Main{
 // 3 4 5 
 // 4 5 
 // 5
+
+public class Pattern {
+    public static void main(String[] args) {
+        int n = 4; 
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }                    
+            System.out.println(); 
+        }
+    }
+}
+
+// output:
+// 1
+// 1 2
+// 1 2 3 
+// 1 2 3 4     
