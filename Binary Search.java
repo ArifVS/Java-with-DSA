2de37cf2d3e7fe927b9ab204a8cd915bf32b9ed3
Arif <v.s.arifulla@gmail.com>
2025-06-28 16:17:04 +0530

    Update Binary Search.java

--- Binary Search.java
+++ Binary Search.java
@@ -1,27 +1,24 @@
 //Binary Search
 import java.util.*;
-
-class binarySearch{
-    static int search(int a[], int sea){
+class Solution {
+    public int search(int[] a, int t) {
         int h = a.length-1;
         int l = 0;
-        int mid=0;
+        int mid = 0;
         while(l<=h){
-            mid = (h+l)/2;
-            if(sea==a[mid]){
-                return a[mid];
-            }else if(sea<mid){
+            mid = (l+h)/2;
+            if(t==a[mid]){
+                return mid;
+            }else if(t<a[mid]){
                 h = mid-1;
-                l = l;
-                
+
             }else{
-                l=mid+1;
-                h = h;
+                l = mid+1;
             }
         }return -1;
+        
     }
 }
-
 class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -30,9 +27,8 @@ class Main{
         for(int i=0;i<n;i++){
             a[i] = sc.nextInt();
         }
-        System.out.print("Enter the element to search:");
-        int sea = sc.nextInt();
-        binarySearch b = new binarySearch();
-        System.out.print(b.search(a,sea));
+        int t = sc.nextInt();
+        Solution s = new Solution();
+        System.out.print(s.search(a,t));
     }
 }
