618183e116643df530d235fae361c9a9b2712b48
ArifVS <v.s.arifulla@gmail.com>
2025-07-17 15:55:58 +0530

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -9,10 +9,24 @@ class Main{
             s.push(e);
         }
         System.out.println("The size of stack is:"+s.size());
+        System.out.println("The top of the element is:"+s.peek());
+        
+        
+        System.out.println("Element to search:");
+        int n1 = sc.nextInt();
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (s.search(n1) != -1) {
+                System.out.println("Element is found at position from top: " + s.search(n1));
+                break;
+            } else {
+                System.out.println("Element is not found");
+                break;
+            }
+        }
         System.out.println("The stack elements are:");
         while(!s.isEmpty()){
             System.out.print(s.pop());
         }
-        
     }
 }
