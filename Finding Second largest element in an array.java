0127c632fd38a42a8253c59581ccdf69384b2dff
ArifVS <v.s.arifulla@gmail.com>
2025-07-20 20:20:28 +0530

    Update Finding Second largest element in an array.java

--- Finding Second largest element in an array.java
+++ Finding Second largest element in an array.java
@@ -1,28 +1,15 @@
 // Finding Second largest element
-import java.util.*;
-class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a[] = new int[n];
-        for(int i=0;i<n;i++){
-            a[i] = sc.nextInt();
+class Solution {
+    public int secondLargestElement(int[] a) {
+    int largest = a[0];
+    int slargest = -1;
+    for(int i=0;i<a.length;i++){
+        if(a[i]>largest){
+            slargest=largest;
+            largest=a[i];
+        }else if(a[i]<largest && a[i]>slargest){
+            slargest = a[i];
         }
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MIN_VALUE;
-        if(a[0]>a[1]){
-            max = a[0];
-            min = a[1];
-        }else{
-            min = a[0];
-            max = a[1];
-        }for(int i=2;i<a.length;i++){
-            if(a[i]>max){
-                min = max;
-                max = a[i];
-            }else if(a[i]>min){
-                min = a[i];
-            }
-        }System.out.print("First Largerst element is:"+max+"Second largest element is:"+min);
+    }return slargest;
     }
 }
