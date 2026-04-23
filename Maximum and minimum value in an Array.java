1053647ea4ea648aeef750dae982afa8c2dffe93
ArifVS <v.s.arifulla@gmail.com>
2026-04-23 15:03:40 +0530

    Create Maximum and minimum value in an Array.java

---
+++ Maximum and minimum value in an Array.java
@@ -0,0 +1,29 @@
+//  Maximum and minimum value in an Array
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for(int i=0;i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0; i<size;i++){
+            System.out.print(arr[i]+" ");
+        }
+        int max = arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+        int min = arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(min>arr[i]){
+                min = arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
